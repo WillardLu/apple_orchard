f105aa344ac5cf5e78e180be6ca848c89ecf39f3
陆巍 <willard.lu@outlook.com>
2023-12-13 14:57:18 +0800

    编写StrReplaceAll函数

--- c/string/StrReplaceAll/main.c
+++ c/string/StrReplaceAll/main.c
@@ -7,16 +7,30 @@
 #include <setjmp.h>
 #include <stdarg.h>
 #include <stddef.h>
+#include <stdio.h>
 #include <cmocka.h>
 
-static void ExportDataTest(void **state) {
+#include "str_replace_all.h"
+
+static void StrReplaceAllTest(void **state) {
+  char *str1 = NULL;
+  char *str2 = NULL;
+  str1 = StrReplaceAll("ffabcffghjff", "ff", "f");
+  str2 = StrReplaceAll("abcghjf", "ff", "f");
+  //printf("%s\n", str1);
+  if (str1 != NULL && str2 != NULL) {
+    assert_string_equal(str1, "fabcfghjf");
+    assert_string_equal(str2, "abcghjf");
+  }
+  free(str1);
+  free(str2);
+  
   return;
 }
 
-
 int main(int argc, char *argv[]) {
   const struct CMUnitTest tests[] = {
-    cmocka_unit_test(ExportDataTest),
+    cmocka_unit_test(StrReplaceAllTest),
   };
   return cmocka_run_group_tests(tests, NULL, NULL);
 }
---
+++ c/string/StrReplaceAll/str_replace_all.h
@@ -0,0 +1,16 @@
+// @copyright Copyright 2023 Willard Lu
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+#ifndef StrReplaceAll_str_replace_all_h_
+#define StrReplaceAll_str_replace_all_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char * StrReplaceAll(char *str, char *old, char *new);
+
+#endif  // StrReplaceAll_str_replace_all_h_
